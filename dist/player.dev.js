6f0039d0482beac2f3e22fe3209605bf64fe3ae8
itIsBrando <mooballygoobally@gmail.com>
2020-08-13 16:18:51 -0400

    Initial commit

---
+++ dist/player.dev.js
@@ -0,0 +1,75 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var map = Array(128 * 64 / 4);
+
+var Game =
+/*#__PURE__*/
+function () {
+  function Game() {
+    _classCallCheck(this, Game);
+  } // initalizes a game
+
+
+  _createClass(Game, null, [{
+    key: "run",
+    value: function run() {
+      peerID = document.getElementById("IDInput").value;
+      console.log(peerID);
+      var connection = peer.connect(peerID); // send a handshake
+
+      connection.on('open', function () {
+        connection.send('hello');
+        console.log('sent hello');
+        players.append(new Player(2));
+      });
+      console.log("started game");
+      inGame = true;
+      players.forEach(function (element) {
+        element.draw();
+      });
+    }
+  }]);
+
+  return Game;
+}();
+
+var Player =
+/*#__PURE__*/
+function () {
+  function Player(number) {
+    _classCallCheck(this, Player);
+
+    this.playerNumber = number;
+    this.x = this.y = 0;
+  }
+
+  _createClass(Player, [{
+    key: "draw",
+    value: function draw() {
+      context.fillStyle = "#0000FF";
+      context.fillRect(this.x, this.y, 8, 8);
+    }
+  }, {
+    key: "clear",
+    value: function clear() {
+      context.fillStyle = "#000000";
+      context.fillRect(this.x, this.y, 8, 8);
+    }
+  }, {
+    key: "move",
+    value: function move(dx, dy) {
+      this.clear();
+      this.x += dx;
+      this.y += dy;
+      this.draw();
+    }
+  }]);
+
+  return Player;
+}();
---
+++ player.js
@@ -0,0 +1,53 @@
+var map = Array(128 * 64 / 4);
+
+class Game {
+    constructor() {
+
+    }
+
+    // initalizes a game
+    static run() {
+        peerID = document.getElementById("IDInput").value;
+        console.log(peerID);
+        let connection = peer.connect(peerID);
+
+        // send a handshake
+        connection.on('open', function() {
+            connection.send('hello');
+            console.log('sent hello');
+            players.append(new Player(2));
+        });
+
+        console.log("started game");
+        inGame = true;
+
+        players.forEach(element => {
+           element.draw(); 
+        });
+    }
+}
+
+
+class Player {
+    constructor(number) {
+        this.playerNumber = number
+        this.x = this.y = 0;
+    }
+
+    draw() {
+        context.fillStyle = "#0000FF"
+        context.fillRect(this.x, this.y, 8, 8);
+    }
+
+    clear() {
+        context.fillStyle = "#000000"
+        context.fillRect(this.x, this.y, 8, 8);
+    }
+
+    move(dx, dy) {
+        this.clear();
+        this.x += dx;
+        this.y += dy;
+        this.draw();
+    }
+}
